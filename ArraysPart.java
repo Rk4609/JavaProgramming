5365144fb35059691817511f7f31a4e116d18eab
Rk4609 <rakeshjangid8742@gmail.com>
2026-03-01 21:18:17 +0530

    Refactor: Comment out code sections for ArraysPart and JavaBasics

--- ArraysPart.java
+++ ArraysPart.java
@@ -21,7 +21,7 @@
 // }
 
 //-----------------------------------------------------------------------------------------
-      //Arrays passing as argument -reference
+//Arrays passing as argument -reference
 
 // import java.util.*;
 
@@ -44,3 +44,128 @@
 
 //     }
 // }
+
+//---------- LinearSearch --------------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class ArraysPart {
+
+//     public static int LinearSearch(int numbers[], int key) {
+//         for (int i = 0; i < numbers.length; i++) {
+//             if (numbers[i] == key) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+//         int key = 16;
+
+//         int index = LinearSearch(numbers, key);
+//         if (index == -1) {
+//             System.out.println("Not Found");
+//         } else {
+//             System.out.println(" Key is at index : " + index);
+//         }
+//     }
+// }
+
+//---------Largest Number Search----------------------------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class ArraysPart {
+
+//     public static int getLargest(int numbers[]){
+//          int largest = Integer.MIN_VALUE;
+//          int smallest= Integer.MAX_VALUE;
+//         for(int i=0; i<numbers.length; i++){
+//             if (largest < numbers[i]) {
+//                 largest= numbers[i];
+//             }
+//             if (smallest > numbers[i]) {
+//                 smallest=numbers[i];
+//             }
+//         }
+//           System.out.println("Smallest value is : " + smallest);
+//             return largest;
+//     }
+//     public static void main(String[]args){
+//         int numbers[]={2,4,3,5,7,9,19,45};
+
+//         System.out.println("largest value is : "+ getLargest(numbers));
+
+//     }
+// }
+
+//--------Arrays reverse------------------------------------------------------------------------------------------------------
+// import java.util.*;
+
+// public class ArraysPart {
+
+//     public static void Reverse(int numbers[]) {
+//         int first = 0;
+//         int last = numbers.length - 1;
+
+//         while (first < last) {
+//             int temp = numbers[last];
+//             numbers[last] = numbers[first];
+//             numbers[first] = temp;
+
+//             first++;
+//             last--;
+//         }
+//     }
+
+//     public static void main(String[] args) {
+
+//         int numbers[] = { 2, 4, 6, 8, 10,12,14,16,17 };
+//         Reverse(numbers);
+//         for (int i = 0; i < numbers.length; i++) {
+//             System.out.print(numbers[i] + " ");
+//         }
+//         System.out.println();
+
+//     }
+// }
+
+//-------------PrintPais of arrays--------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class ArraysPart {
+
+//     public static void printPairs(int numbers[]) {
+//         int tp = 0;
+//         for (int i = 0; i < numbers.length; i++) {
+//             int curr = numbers[i]; /// 2,4,6,8,10,12
+//             for (int j = i + 1; j < numbers.length; j++) {
+//                 System.out.print("(" + curr + "," + numbers[j] + ")");
+//                 tp++;
+//             }
+//             System.out.println();
+//         }
+//         System.out.println("total numbers of pais :" + tp);
+//     }
+
+//     public static void main(String[] args) {
+//         int numbers[] = { 2, 4, 6, 8, 10 };
+//         printPairs(numbers);
+//     }
+// }
+
+
+//-------------------------------------------------------------------------------------
+
+import java.util.*;
+
+public class ArraysPart {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
--- JavaBasics.java
+++ JavaBasics.java
@@ -272,36 +272,36 @@
 // }
 
 
-import java.util.*;
+// import java.util.*;
 
-public class JavaBasics {
+// public class JavaBasics {
 
-    public static void diamond(int n) {
+//     public static void diamond(int n) {
       
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=(n-i); j++){
-                System.out.print(" ");
-            }
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=(n-i); j++){
+//                 System.out.print(" ");
+//             }
 
-            for(int j=1; j<=(2*i)-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-           for(int i=n; i>=1; i--){
-            for(int j=1; j<=(n-i); j++){
-                System.out.print(" ");
-            }
+//             for(int j=1; j<=(2*i)-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//            for(int i=n; i>=1; i--){
+//             for(int j=1; j<=(n-i); j++){
+//                 System.out.print(" ");
+//             }
 
-            for(int j=1; j<=(2*i)-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+//             for(int j=1; j<=(2*i)-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
 
-    public static void main(String[] args) {
-      diamond(5);
+//     public static void main(String[] args) {
+//       diamond(5);
         
-    }
-}
+//     }
+// }
