010fc8918a12c9d3a4a1872154db4c3f6993dcf5
Rk4609 <rakeshjangid8742@gmail.com>
2026-03-01 09:37:56 +0530

    ArraysDecla

--- ArraysPart.java
+++ ArraysPart.java
@@ -1,11 +1,24 @@
-import java.util.*;
+// import java.util.*;
 
-public class ArraysPart {
+// public class ArraysPart {
     
 
 
- public static void main(String[] args) {
+//  public static void main(String[] args) {
+//     int marks[]= new int[50];
+
+//     Scanner sc = new Scanner(System.in);
+//     marks[0]= sc.nextInt();
+//     marks[1]=sc.nextInt();
+//     marks[2]=sc.nextInt();
     
+//     System.out.println("phy : " + marks[0]);
+//     System.out.println("che : " + marks[1]);
+//     System.out.println("bio : " + marks[2]);
+
+//     int percentage = (marks[0]+marks[1]+marks[2]) / 3;
+//     System.out.println("percentage : "+percentage);
+
         
-    }
-}
+//     }
+// }
