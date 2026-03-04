1d27e395d162cba0c7d62a8c88e3cda3b8ccdfbd
Rk4609 <rakeshjangid8742@gmail.com>
2026-03-04 23:18:05 +0530

    Add Sorting class with BubbleSort and PrintArr methods

--- ArraysPart.java
+++ ArraysPart.java
@@ -426,4 +426,5 @@
 //     int prices[]={7,1,5,3,6,4};
 //     System.out.println(maxProfit(prices));
 //  }
-// }
+// }  
+
---
+++ Sorting.java
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------------
+// { bubble sort }
+// import java.util.*;
+
+// public class Sorting {
+         
+//     public static void BubbleSort(int arr[]){
+//         for(int turn=0; turn<arr.length; turn++){
+//             for(int j=0; j<arr.length-1-turn; j++){
+//                 if (arr[j] > arr[j+1]) {
+//                     // Swap
+
+//                     int temp= arr[j];
+//                     arr[j]= arr[j+1];
+//                     arr[j+1] =temp;
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void PrintArr(int arr[]){
+//         for(int i=0; i<arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[]args){
+//           int arr[]={5,4,1,3,2};
+//           BubbleSort(arr);
+//             PrintArr(arr);
+            
+//     }
+// }
+
+//-----------------------------------------------------------------------------------------
