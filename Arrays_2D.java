fc7fe101d3115be5ba555a9102a18c7ffd3e21a1
Rk4609 <rakeshjangid8742@gmail.com>
2026-03-11 23:36:58 +0530

    Add methods for calculating diagonal sum, staircase search, palindrome check, and shortest path in string to Arrays_2D class

--- Arrays_2D.java
+++ Arrays_2D.java
@@ -97,3 +97,124 @@
 //     }
 // }
 
+//--------{Diagonal sum }-----------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class Arrays_2D {
+
+//     public static int diagonalSum(int matrix[][]) {
+//         int sum = 0;
+//         for (int i = 0; i < matrix.length; i++) {
+
+//             // primary
+//             sum += matrix[i][i];
+//             // secondary
+//             if (i != matrix.length - 1 - i) {
+//                 sum += matrix[i][matrix.length - i - 1];
+//             }
+
+//         }
+//         return sum;
+//     }
+
+//     public static void main(String[] args) {
+
+//         int matrix[][] = { { 1, 2, 3, 4 },
+//                 { 5, 6, 7, 8 },
+//                 { 9, 10, 11, 12 },
+//                 { 13, 14, 15, 16 } };
+
+//         System.out.println(diagonalSum(matrix));
+//     }
+// }
+
+//-------------------------------------------------------------------------------------
+// import java.util.*;
+
+// public class Arrays_2D {
+
+//     public static boolean staircaseSearch(int matrix[][], int key) {
+//         int row = 0, col = matrix[0].length - 1;
+
+//         while (row < matrix.length && col >= 0) {
+//             if (matrix[row][col] == key) {
+//                 System.out.println("Found key at (" + row + "," + col + ")");
+//                 return true;
+//             } else if (key < matrix[row][col]) {
+//                 col--;
+//             } else {
+//                 row++;
+//             }
+//         }
+//         System.out.println(" Key is not found");
+//         return false;
+
+//     }
+
+//     public static void main(String[] args) {
+//         int matrix[][] = { { 10, 20, 30, 40 },
+//                 { 15, 25, 35, 45 },
+//                 { 27, 29, 37, 48 },
+//                 { 32, 33, 39, 50 } };
+
+//         int key = 33;
+//         staircaseSearch(matrix, key);
+//     }
+// }
+
+//------{Palindrome}------------------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class Arrays_2D {
+//     public static boolean isPalindrome(String str) {
+//         for (int i = 0; i < str.length() / 2; i++) {
+//             int n = str.length();
+
+//             if (str.charAt(i) != str.charAt(n - 1 - i)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public static void main(String[] args) {
+//         String str = "racecar";
+//         System.out.println(isPalindrome(str));
+//     }
+// } 
+
+//---------{Shortest path in string}------------------------------------------------------------------------
+
+// import java.util.*;
+
+// public class Arrays_2D {
+//     public static float getShortestpath(String path) {
+//         int x = 0, y = 0;
+//         for (int i = 0; i < path.length(); i++) {
+//             char dir = path.charAt(i);
+
+//             // South
+//             if (dir == 'S') {
+//                 y--;
+//             }
+//             // North
+//             else if (dir == 'N') {
+//                 y++;
+//             } else if (dir == 'W') {
+//                 x--;
+//             } else {
+//                 x++;
+//             }
+//         }
+//         int X2 = x * x;
+//         int Y2 = y * y;
+//         return (float) Math.sqrt(X2 + Y2);
+//     }
+
+//     public static void main(String[] args) {
+//         String path = "WNEENESENNN";
+//         System.out.println(getShortestpath(path));
+//     }
+// }
