0aa0790fcf4b95908f307570dda8fb39a0822580
Rk4609 <rakeshjangid8742@gmail.com>
2026-03-15 23:22:21 +0530

    Add RecursionBasics class with methods for printing numbers in decreasing and increasing order

---
+++ RecursionBasics.java
@@ -0,0 +1,47 @@
+//---------------------------------------------
+//-----------Problem -1 [print numbers from n to 1(Decresing order)]
+// import java.util.*;
+// public class RecursionBasics {
+
+//     public static void printDec(int n) {
+//         if (n == 1) {
+//             System.out.print(1);
+//             return;
+//         }
+//         System.out.print(n + " ");
+//         printDec(n-1);
+
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 10;
+//         printDec(n);
+//     }
+// }
+
+//--------------------------------------------------------------------------------------------------------------------
+//-----------Problem -2 [print numbers from n to 1(Incresing order)]
+
+// import java.util.*;
+
+// public class RecursionBasics {
+
+//     public static void printIncre(int n) {
+//         if (n == 1) {
+//             System.out.print(n+" ");
+//             return;
+//         }
+
+//         printIncre(n - 1);
+//         System.out.print(n + " ");
+
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 10;
+//         // printDec(n);
+//         printIncre(n);
+//     }
+// }
+
+//------------------------------------------------------------------------------------------------
